a422a140c028823d5e65f38c248367fb22fd261a
Aaron Bockover <aaron@abock.dev>
2020-06-10 14:36:08 -0400

    Add CNAME

--- docs.sh
+++ docs.sh
@@ -7,5 +7,8 @@ nuget install -OutputDirectory _nuget docfx.console -Version $docfx_version
 
 rm -rf docs docfx/obj src/Ecoji/obj/xdoc
 
+mkdir -p docs
+echo -n ecoji.dev > docs/CNAME
+
 mono $docfx docfx/docfx.json
 mono $docfx build -s docfx/docfx.json
